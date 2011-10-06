4b628cb74ed87c09d946801fe710dbc32fe0f2d1
Sho Shimizu <osho@galibier.org>
2011-10-06 20:30:55 +0800

    Optimized the static import statements

--- src/test/java/org/galibier/packet/TestProtocolNumber.java
+++ src/test/java/org/galibier/packet/TestProtocolNumber.java
@@ -25,9 +25,9 @@
 
 package org.galibier.packet;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestProtocolNumber {
     @Test
