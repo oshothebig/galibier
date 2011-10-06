12fda7666535b9bc3ef6985e4f3e469008fbe30e
Sho Shimizu <osho@galibier.org>
2011-10-06 16:32:24 +0800

    Class and unit test for MAC address is added

---
+++ src/main/java/org/galibier/packet/MACAddress.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2011, Sho SHIMIZU
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.galibier.packet;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+
+import java.util.Arrays;
+
+public class MACAddress {
+    private static final int MAC_ADDRESS_LENGTH = 6;
+    private final byte[] address = new byte[MAC_ADDRESS_LENGTH];
+
+    //  preventing to create a instance
+    private MACAddress() {}
+
+    public static MACAddress getByAddress(String address) {
+        String[] elements = address.split(":");
+        Preconditions.checkArgument(elements.length == MAC_ADDRESS_LENGTH);
+
+        MACAddress instance = new MACAddress();
+        for (int i = 0; i < MAC_ADDRESS_LENGTH; i++) {
+            String element = elements[i];
+            instance.address[i] = (byte)Integer.parseInt(element, 16);
+        }
+
+        return instance;
+    }
+
+    public static MACAddress getByAddress(byte[] address) {
+        Preconditions.checkArgument(address.length == MAC_ADDRESS_LENGTH);
+
+        MACAddress instance = new MACAddress();
+        for (int i = 0; i < MAC_ADDRESS_LENGTH; i++) {
+            instance.address[i] = address[i];
+        }
+
+        return instance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof MACAddress) {
+            MACAddress other = (MACAddress)o;
+            return Arrays.equals(this.address, other.address);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(this.address);
+    }
+}
---
+++ src/test/java/org/galibier/packet/TestMACAddress.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2011, Sho SHIMIZU
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.galibier.packet;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMACAddress {
+    @Test
+    public void getByAddress() {
+        MACAddress sa1 = MACAddress.getByAddress("00:01:02:03:04:05");
+        MACAddress ba1 = MACAddress.getByAddress(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04, 0x05});
+        assertEquals(sa1, ba1);
+
+        MACAddress sa2 = MACAddress.getByAddress("FF:FE:FD:10:20:30");
+        MACAddress ba2 = MACAddress.getByAddress(new byte[]{(byte) 0xFF, (byte) 0xFE, (byte) 0xFD, 0x10, 0x20, 0x30});
+        assertEquals(sa2,ba2);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void illegalFormat() {
+        MACAddress addr = MACAddress.getByAddress("0T:00:01:02:03:04");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void longStringFields() {
+        MACAddress addr = MACAddress.getByAddress("00:01:02:03:04:05:06");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shortStringFields() {
+        MACAddress addr = MACAddress.getByAddress("00:01:02:03:04");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getAddressArgumentCount2() {
+        MACAddress addr = MACAddress.getByAddress(new byte[]{0x01, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06});
+    }
+}
